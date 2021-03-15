0c3a3c77400bc4a61e05fb2968179a0bc67d1b25
norrw <willnorrislenovo@outlook.com>
2021-03-15 10:47:05 +0000

    Added styling and logic for home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,15 @@
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Post from './components/Post';
+import Project from './components/Project';
+import SinglePost from './components/SinglePost';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route component={Home} path='/' exact />
         <Route component={About} path='/about' />
